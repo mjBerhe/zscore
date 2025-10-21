2cbf6123910c12997794778ed6332153ed25844b
mjBerhe <matthew.berhe1@gmail.com>
2025-10-21 01:14:35 -0400

    feat: projections schema update + projections basic ui

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,170 @@
+import { useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import { getProjectionsBySource, getProjectionSets } from '@/server/projections'
 import { createFileRoute } from '@tanstack/react-router'
+import { useServerFn } from '@tanstack/react-start'
+
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOption,
+  ListboxOptions,
+} from '@headlessui/react'
+import { ChevronDown } from 'lucide-react'
+import clsx from 'clsx'
 
 export const Route = createFileRoute('/')({
   component: App,
 })
 
 function App() {
+  const getAllProjectionSets = useServerFn(getProjectionSets)
+  const getProjectionSource = useServerFn(getProjectionsBySource)
+
+  const [selectedProjectionSet, setSelectedProjectionSet] = useState<
+    string | null
+  >(null)
+
+  const {
+    data: projectionSets,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ['projectionSets'],
+    queryFn: () => getAllProjectionSets(),
+  })
+
+  const {
+    data: selectedProjections,
+    isLoading: projectionsLoading,
+    error: projectionsError,
+  } = useQuery({
+    queryKey: ['projections', selectedProjectionSet],
+    queryFn: () =>
+      selectedProjectionSet
+        ? getProjectionSource({ data: { source: selectedProjectionSet } })
+        : [],
+    enabled: !!selectedProjectionSet, // only run when a source is selected
+  })
+
+  if (isLoading) return <p className="p-4 text-gray-400">Loading sets...</p>
+  if (error) return <p className="p-4 text-red-400">Error loading sets.</p>
+
   return (
     <div className="min-h-screen bg-black text-white">
-      <div>hello this is our first deployment</div>
+      <div className="max-w-7xl mx-auto mt-4 p-2">
+        <Listbox
+          value={selectedProjectionSet}
+          onChange={(val) => setSelectedProjectionSet(val)}
+        >
+          <ListboxButton
+            className={clsx(
+              'relative block w-full rounded-lg bg-zinc-800 py-1.5 pr-8 pl-3 text-left text-sm/6 text-white',
+              'focus:not-data-focus:outline-none data-focus:outline-2 data-focus:-outline-offset-2 data-focus:outline-white/25',
+            )}
+          >
+            {selectedProjectionSet ?? 'Select a Projection Set'}
+            <ChevronDown className="absolute right-2 top-2.5 h-4 w-4 text-gray-400" />
+          </ListboxButton>
+          <ListboxOptions
+            anchor="bottom"
+            transition
+            className={clsx(
+              'w-(--button-width) rounded-xl border border-white/5 bg-white/5 p-1 [--anchor-gap:--spacing(1)] focus:outline-none',
+              'transition duration-100 ease-in data-leave:data-closed:opacity-0',
+            )}
+          >
+            {projectionSets?.map((set) => (
+              <ListboxOption
+                key={set}
+                value={set}
+                className="group flex cursor-default items-center gap-2 rounded-lg px-3 py-1.5 select-none data-focus:bg-white/10"
+              >
+                <div className="text-sm text-white">{set}</div>
+              </ListboxOption>
+            ))}
+          </ListboxOptions>
+        </Listbox>
+
+        <div className="space-y-1 mt-2">
+          {selectedProjections && selectedProjections.length > 0 && (
+            <div className="mt-4 overflow-x-auto">
+              <table className="min-w-full table-auto border-collapse border border-gray-700">
+                <thead>
+                  <tr className="bg-zinc-900">
+                    <th className="border border-gray-700 px-2 py-1 text-left text-sm">
+                      Player
+                    </th>
+                    <th className="border border-gray-700 px-2 py-1 text-sm">
+                      PTS
+                    </th>
+                    <th className="border border-gray-700 px-2 py-1 text-sm">
+                      TPM
+                    </th>
+                    <th className="border border-gray-700 px-2 py-1 text-sm">
+                      REB
+                    </th>
+                    <th className="border border-gray-700 px-2 py-1 text-sm">
+                      AST
+                    </th>
+                    <th className="border border-gray-700 px-2 py-1 text-sm">
+                      STL
+                    </th>
+                    <th className="border border-gray-700 px-2 py-1 text-sm">
+                      BLK
+                    </th>
+                    <th className="border border-gray-700 px-2 py-1 text-sm">
+                      TOV
+                    </th>
+                    <th className="border border-gray-700 px-2 py-1 text-sm">
+                      FG%
+                    </th>
+                    <th className="border border-gray-700 px-2 py-1 text-sm">
+                      FT%
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {selectedProjections.map((row, i) => (
+                    <tr key={i} className="even:bg-zinc-800 odd:bg-zinc-900">
+                      <td className="border border-gray-700 px-2 py-1 text-sm">
+                        {row.playerName}
+                      </td>
+                      <td className="border border-gray-700 px-2 py-1 text-sm">
+                        {row.pts}
+                      </td>
+                      <td className="border border-gray-700 px-2 py-1 text-sm">
+                        {row.tpm}
+                      </td>
+                      <td className="border border-gray-700 px-2 py-1 text-sm">
+                        {row.reb}
+                      </td>
+                      <td className="border border-gray-700 px-2 py-1 text-sm">
+                        {row.ast}
+                      </td>
+                      <td className="border border-gray-700 px-2 py-1 text-sm">
+                        {row.stl}
+                      </td>
+                      <td className="border border-gray-700 px-2 py-1 text-sm">
+                        {row.blk}
+                      </td>
+                      <td className="border border-gray-700 px-2 py-1 text-sm">
+                        {row.to}
+                      </td>
+                      <td className="border border-gray-700 px-2 py-1 text-sm">
+                        {row.fgp}
+                      </td>
+                      <td className="border border-gray-700 px-2 py-1 text-sm">
+                        {row.ftp}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
--- src/server/projections.ts
+++ src/server/projections.ts
@@ -1,6 +1,7 @@
 import { createServerFn } from '@tanstack/react-start'
 import { db } from '..'
-import { projections, players } from '@/db/schema'
+import { projections } from '@/db/schema'
+import { eq } from 'drizzle-orm'
 
 export const uploadProjections = createServerFn({ method: 'POST' })
   .inputValidator((data: { csv: string }) => data)
@@ -18,39 +19,59 @@ export const uploadProjections = createServerFn({ method: 'POST' })
     const formatted = rows.map((r: any) => ({
       playerName: r['Player Name'],
       season: 2025,
-      source: 'MyProjections',
+      source: "Matt's 2025-2026 Projections",
+
       gp: Number(r['GP'] ?? 0),
-      fgPct: Number(r['FG%'] ?? 0),
-      ftPct: Number(r['FT%'] ?? 0),
-      threepm: Number(r['TPM'] ?? 0),
+      pts: Number(r['PTS'] ?? 0),
+      tpm: Number(r['TPM'] ?? 0),
       reb: Number(r['REB'] ?? 0),
       ast: Number(r['AST'] ?? 0),
       stl: Number(r['STL'] ?? 0),
       blk: Number(r['BLK'] ?? 0),
       to: Number(r['TOV'] ?? 0),
-      pts: Number(r['PTS'] ?? 0),
+      fgp: Number(r['FG%'] ?? 0),
+      fga: Number(r['FGA'] ?? 0),
+      ftp: Number(r['FT%'] ?? 0),
+      fta: Number(r['FTA'] ?? 0),
     }))
 
-    // --- Insert or upsert ---
     for (const row of formatted) {
       await db.insert(projections).values(row)
-      // .onConflictDoUpdate({
-      //   target: [projections.playerName, projections.season],
-      //   set: {
-      //     source: row.source,
-      //     gp: row.gp,
-      //     fgPct: row.fgPct,
-      //     ftPct: row.ftPct,
-      //     threepm: row.threepm,
-      //     reb: row.reb,
-      //     ast: row.ast,
-      //     stl: row.stl,
-      //     blk: row.blk,
-      //     to: row.to,
-      //     pts: row.pts,
-      //   },
-      // })
     }
 
     return { success: true, count: formatted.length }
   })
+
+export const getProjectionSets = createServerFn({ method: 'GET' }).handler(
+  async () => {
+    const rows = await db
+      .selectDistinctOn([projections.source])
+      .from(projections)
+
+    return rows.map((r) => r.source)
+  },
+)
+
+export const getProjectionsBySource = createServerFn({ method: 'GET' })
+  .inputValidator((data: { source: string }) => data)
+  .handler(async ({ data }) => {
+    const rows = await db
+      .select()
+      .from(projections)
+      .where(eq(projections.source, data.source))
+    return rows
+  })
+
+export const getAllProjections = createServerFn({ method: 'GET' }).handler(
+  async () => {
+    const rows = await db.select().from(projections)
+    return rows
+  },
+)
+
+// export const getAllPlayers = createServerFn({ method: 'GET' }).handler(
+//   async () => {
+//     const result = await db.select().from(players)
+//     return result
+//   },
+// )
